d5ce1de71cd7b14abfb2d54d8651b329ca6969d5
Krista Shuckerow <kshuckerow@gmail.com>
2016-09-22 20:07:46 -0400

    Basic Time Series Analysis
    Doing moving averages, seasonal and non seasonal data. Separating trends and seasonality and error.

---
+++ TimeSeries Practice.R
@@ -0,0 +1,23 @@
+#plot original time series with seasonal component
+plot.ts(AirPassengers, nc = 12) #how many months -- works without nc component
+
+APsma<- SMA(AirPassengers, n = 12) #take smooth moving average
+plot.ts(APsma) #If you don't do as .ts it will show points instead of a line
+
+#pull appart sources of variation
+APBD <- decompose(AirPassengers, type = "multiplicative") #multiplicative because seasonal oscillation appears to be growing has significanly less variation than additive
+plot(APBD) #shows original, trend, error, and seasonal component
+
+#graph of trends (includes error but not seasonal changes)
+SeasonAdjusted <- (AirPassengers/APBD$seasonal) #Because it's multiplicative divide... if it were additive you would multiply
+plot.ts(SeasonAdjusted)
+
+#simple exponential smoothing. 
+#Best for additive models with now seasonal changes
+#something that fluctuates around a poitn ovr time is ideal
+EXPsmoothed <- HoltWinters(discoveries, beta = F, gamma = F)
+plot.ts(discoveries)
+plot(EXPsmoothed)
+
+plot(iris$Sepal.Width, iris$Sepal.Length, fill.colour := red)
+?plot()
